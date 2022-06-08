2be23a020be59b0f1dcc834e3830450e2dc4e97a
Pascal <spezifisch@users.noreply.github.com>
2022-06-08 22:43:27 +0200

    cpp

---
+++ remove-palindromic-subsequences/v2.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    int removePalindromeSub(string s) {
+        int a = 0, b = s.size() - 1;
+        while (a < b) {
+            if (s[a] != s[b]) {
+                return 2;
+            }
+            a++;
+            b--;
+        }
+        return 1;
+    }
+};
+
+// Runtime: 3 ms, faster than 38.93% of C++ online submissions for Remove Palindromic Subsequences.
+// Memory Usage: 6.1 MB, less than 79.13% of C++ online submissions for Remove Palindromic Subsequences.
