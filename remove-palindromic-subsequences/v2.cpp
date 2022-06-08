aeb97e12ffe736b47ba82bc87f0013ffb7e4a48a
Pascal <spezifisch@users.noreply.github.com>
2022-06-08 22:53:28 +0200

    add clang-format

--- remove-palindromic-subsequences/v2.cpp
+++ remove-palindromic-subsequences/v2.cpp
@@ -1,17 +1,18 @@
 class Solution {
-public:
-    int removePalindromeSub(string s) {
-        int a = 0, b = s.size() - 1;
-        while (a < b) {
-            if (s[a] != s[b]) {
-                return 2;
-            }
-            a++;
-            b--;
-        }
-        return 1;
+ public:
+  int removePalindromeSub(string s) {
+    int a = 0, b = s.size() - 1;
+    while (a < b) {
+      if (s[a] != s[b]) {
+        return 2;
+      }
+      a++;
+      b--;
     }
+    return 1;
+  }
 };
 
-// Runtime: 3 ms, faster than 38.93% of C++ online submissions for Remove Palindromic Subsequences.
-// Memory Usage: 6.1 MB, less than 79.13% of C++ online submissions for Remove Palindromic Subsequences.
+// Runtime: 3 ms, faster than 38.93% of C++ online submissions for Remove
+// Palindromic Subsequences. Memory Usage: 6.1 MB, less than 79.13% of C++
+// online submissions for Remove Palindromic Subsequences.
